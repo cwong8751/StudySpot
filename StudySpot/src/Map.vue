3249794924d1406aa2622e4c5744be48da8806e7
Carl <112435622+cwong8751@users.noreply.github.com>
2024-02-01 12:22:09 -0600

    map shows

---
+++ StudySpot/src/Map.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet"
+import { latLng } from "leaflet"
+
+// declare map variables
+const zoom = 16.2
+const center = latLng(38.648987, -90.312553);
+const url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+const attribution = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+</script>
+
+<template>
+  <main>
+    <div class="wrapper">
+      <!-- initialize map -->
+      <l-map style="height: 400px" :center="center" :zoom="zoom">
+        <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+      </l-map>
+
+      <!-- get list of tables -->
+      <h2>Tabling information</h2>
+      <p>I am a placeholder</p>
+    </div>
+  </main>
+</template>
--- StudySpot/src/main.js
+++ StudySpot/src/main.js
@@ -1,15 +1,17 @@
-//import './assets/main.css'
+import 'leaflet/dist/leaflet.css';
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import Login from './Login.vue'
 import Hello from './Hello.vue'
+import Map from './Map.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 // build routes
 const routes = [
     {path: '/', component: Hello},
-    {path: '/login', component: Login}
+    {path: '/login', component: Login},
+    {path: '/map', component: Map}
 ]
 
 // build router
